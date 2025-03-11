010ec71dd99099667f51c62c81d274914e88d239
Peter Domokos <petedomokos@gmail.com>
2025-03-11 18:17:34 +0000

    added quadrantsBarChartComponent

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,18 @@
 import { useState } from 'react';
 import Header from './components/header/page';
 import { exampleItems } from './data/pageContent';
+import QuadrantsBarChartVisual from './quadrantsBarChartVisual/page';
+import { getRehabDataForVisuals, createMockDataForVisuals } from './data/mockData';
 
 export default function Home() {
   const [selectedExample, setSelectedExample] = useState(exampleItems[0].key);
+  const data = selectedExample === 'mock-dataset-500' ? createMockDataForVisuals(500) : getRehabDataForVisuals(24);
   
   return (
     <div className="app">
       <Header menuItems={exampleItems} selected={selectedExample} onSelect={setSelectedExample} />
       <main className="main">
-        Main Content
+      <QuadrantsBarChartVisual data={data}/>
       </main>
     </div>
   );
